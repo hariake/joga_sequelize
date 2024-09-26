9bce4843eaff2d43a3c7b83b4f0256cb5e2ef31f
Hariake <tosarv@gmail.com>
2024-09-26 11:41:56 +0300

    Getted article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -3,14 +3,20 @@ const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequeli
 
 const Article = require('../models/article')(sequelize, Sequelize.DataTypes);
 
-
+//kogu data
 const getAllArticles = async (req, res) => {
     const articles = await Article.findAll()
     res.json({articles: articles})
 } 
 
+//data slugi kaupa
+const getArticleBySlug = async (req, res) => {
+    const article = await Article.findOne()
+    res.json({article: article})
+} 
 
 
 module.exports= {
-    getAllArticles
+    getAllArticles,
+    getArticleBySlug
 };  
