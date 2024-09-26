2b2ddcbe3a840a01814f7e6546e6575505617987
Hariake <tosarv@gmail.com>
2024-09-26 09:13:39 +0300

    Created article table and added table data

---
+++ seeders/20240924122203-demo-articles.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Introduction to Ashtanga',
+        slug: 'introduction-to-ashtanga',
+        image: 'ashtanga.jpg',
+        body: '<p> AAAAAAAAAAAAAAAAAAAAAAAA </p>',
+        published: '2020-01-08 15:04:23',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Mornging vinyasa flow routine',
+        slug: 'morning-vinyasa-flow-routine',
+        image: 'morning.jpg',
+        body: '<p> AAAAAAAAAAAAAAAAAAAAAAAA </p>',
+        published: '2020-01-08 15:04:23',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Secrets of a yoga teacher',
+        slug: 'secrets-of-a-yoga-teacher',
+        image: 'yoga-teacher.jpg',
+        body: '<p> AAAAAAAAAAAAAAAAAAAAAAAA </p>',
+        published: '2020-01-08 15:04:23',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Articles', null, {});
+  }  
+};
